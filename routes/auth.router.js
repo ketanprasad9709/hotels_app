7a60f9b19b129fe34606b24f40befb959c87fed7
ketanprasad9709 <158566774+ketanprasad9709@users.noreply.github.com>
2025-01-20 09:15:09 +0530

    Update auth.router.js

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const signuphandler = require("../controllers/signupController");
 const loginhandler = require("../controllers/logincontroller");
+const signupTesthandler = require("../controllers/signupTestcontroller");
 
 const router = express.Router();
 
@@ -10,6 +11,9 @@ router.route("/register")
 
 router.route("/login")
     .post(loginhandler);
+
+router.route("/signuptest")
+    .post(signupTesthandler);
 
 module.exports = router;
 
