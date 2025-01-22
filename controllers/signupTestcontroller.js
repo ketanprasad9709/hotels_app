128174cbdc95c75274569c9a04bc44fcf812c062
ketanprasad9709 <158566774+ketanprasad9709@users.noreply.github.com>
2025-01-22 07:51:14 +0530

    Update signupTestcontroller 3.js

--- controllers/signupTestcontroller.js
+++ controllers/signupTestcontroller.js
@@ -5,11 +5,13 @@ const signupTesthandler = async(req, res) => {
     try{
         const user = await User.findOne({ $or: [{ number: req.body.number }, { email: req.body.email }]});
         
-        if(!user){
+        if(!user.userdetail){
           return res.status(401).json({exists: false, message: "User not found"});
-        }
+        };
 
-        return res.status(200).json({ exists: true, message: "User exixts" });
+        if(user){
+            return res.status(200).json({ exists: true, message: "User exixts" });
+        };
     } catch(err){
         res.json({ message: "Data for credendials check not posted" });
     };
